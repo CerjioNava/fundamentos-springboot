3b53e4130a1a2353cc8f7a7ffae1a8659dd01b92
unknown <cerjionava@gmail.com>
2021-08-08 11:32:16 -0400

    3ra seccion lista

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,9 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,21 +15,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
 	// Inyección de dependencias
 	private final ComponentDependency componentDependency;
 	private final MyBean myBean;
 	private final MyBeanWithDependency myBeanWithDependency;
 	private final MyBeanWithProperties myBeanWithProperties;
+	private final UserPojo userPojo;
 
 	public FundamentosApplication(
 			@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 			MyBean myBean,
 			MyBeanWithDependency myBeanWithDependency,
-			MyBeanWithProperties myBeanWithProperties) {
+			MyBeanWithProperties myBeanWithProperties,
+			UserPojo userPojo) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -39,5 +47,13 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
+		try {
+			// error
+			int value = 10/0;
+			LOGGER.debug("Mi valor: " + value);		// No se imprime ya que se catchea el error primero.
+		} catch (Exception e) {
+			LOGGER.error("Esto es un error al dividir por cero 0 " + e.getMessage());
+		}
 	}
 }
